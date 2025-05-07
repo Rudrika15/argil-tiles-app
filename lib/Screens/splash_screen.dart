7bf409ac1ac726ac093c3fe5fdfae3336c4728b1
PruthvirajSinh8080 <jadejapruthviraj8080@gmail.com>
2025-05-07 11:16:29 +0530

    changed background color

--- lib/Screens/splash_screen.dart
+++ lib/Screens/splash_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:argil_tiles/Screens/HomeScreen.dart';
 import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -26,15 +27,15 @@ class SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        color: Colors.grey[400],
+        color: Color(0xFFD3C8BA),
         child: Stack(
           children: [
             // Centered Logo
             Center(
               child: Image.asset(
                 'assets/images/logo.png',
-                width: 150,
-                height: 150,
+                width: 40.w,
+                height: 15.h,
               ),
             ),
           ],
